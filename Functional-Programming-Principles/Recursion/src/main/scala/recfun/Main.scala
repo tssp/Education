86ece3e4965d4cff52b09163ad4dcf1aa10f62bc
tssp <tssp@web.de>
2014-05-08 19:55:46 +0200

    course 'Functional Programming' added Pascal’s Triangle (second iteration)

--- Functional-Programming-Principles/Recursion/src/main/scala/recfun/Main.scala
+++ Functional-Programming-Principles/Recursion/src/main/scala/recfun/Main.scala
@@ -16,32 +16,17 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = {
 
-    require(c >= 0, "Column must be a positive value")
-    require(r >= 0, "Row must be a positive value")
-    require(c <= r, "Column must be less or equal to the row number")
-
-    def pascalIter(currentRow: Int, line: Array[Int]): Int = {
-
-      if(currentRow == r) line(c)
-      else {
-
-        val colsPerLine = line.size + 1
-        val newLine = new Array[Int](colsPerLine)
-
-        newLine(0)=1
-        newLine(colsPerLine-1)=1
-
-        // TODO: Replace for-loop and use a more elegant solution
-        for(i <- 0 until line.size - 1) {
-
-          newLine(1+i) = line(i) + line(i+1)
-        }
-
-        pascalIter(currentRow+1, newLine)
-      }
-    }
-
-    pascalIter(0, Array(1))
+    /*
+             1        1
+            1 1       1 1
+           1 2 1      1 2 1 
+          1 3 3 1     1 3 3 1
+         1 4 6 4 1    1 4 6 4 1
+    */
+    
+    if(c == 0 || r == c) 1
+    else if(c < 0 || r < 0) 0
+    else pascal(c - 1, r - 1) +  pascal(c, r - 1)
   }
 
   /**
--- Functional-Programming-Principles/Recursion/src/test/scala/recfun/PascalSuite.scala
+++ Functional-Programming-Principles/Recursion/src/test/scala/recfun/PascalSuite.scala
@@ -9,12 +9,16 @@ import org.scalatest.junit.JUnitRunner
 class PascalSuite extends FunSuite {
   import Main.pascal
 
-  test("pascal: invalid boundaries") {
+  test("pascal: col=0,row=0") {
+    assert(pascal(0,0) === 1)
+  }
 
-    intercept[IllegalArgumentException] { assert(pascal(-1,0) === 0) }
-    intercept[IllegalArgumentException] { assert(pascal(0,-1) === 0) }
-    intercept[IllegalArgumentException] { assert(pascal(3,2) === 0) }
-    
+  test("pascal: col=0,row=1") {
+    assert(pascal(0,1) === 1)
+  }
+
+  test("pascal: col=1,row=1") {
+    assert(pascal(1,1) === 1)
   }
 
   test("pascal: col=0,row=2") {
