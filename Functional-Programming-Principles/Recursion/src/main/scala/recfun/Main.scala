6e07f302f7a7c458e7bb345986e3d4251f1c9a22
tssp <tssp@web.de>
2014-05-09 09:34:09 +0200

    course 'Functional Programming' added Parentheses Balancing

--- Functional-Programming-Principles/Recursion/src/main/scala/recfun/Main.scala
+++ Functional-Programming-Principles/Recursion/src/main/scala/recfun/Main.scala
@@ -23,7 +23,7 @@ object Main {
           1 3 3 1     1 3 3 1
          1 4 6 4 1    1 4 6 4 1
     */
-    
+
     if(c == 0 || r == c) 1
     else if(c < 0 || r < 0) 0
     else pascal(c - 1, r - 1) +  pascal(c, r - 1)
@@ -32,7 +32,24 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    /*
+    I told him (that it’s not (yet) done). (But he wasn’t listening)
+    */
+
+    def balanceIter(remaining: List[Char], acc: Int): Boolean = {
+
+      if(acc < 0) false
+      else if(remaining.isEmpty && acc != 0) false
+      else if(remaining.isEmpty && acc == 0) true
+      else if(remaining.head == '(') balanceIter(remaining.tail, acc+1)
+      else if(remaining.head == ')') balanceIter(remaining.tail, acc-1)
+      else balanceIter(remaining.tail, acc)
+    }
+
+    balanceIter(chars, 0)
+  }
 
   /**
    * Exercise 3
