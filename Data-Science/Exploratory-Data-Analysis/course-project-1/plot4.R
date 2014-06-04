2808854408efb9ae1093668c9c680129c3f24df9
tssp <tssp@web.de>
2014-06-04 20:35:24 +0200

    course 'Exploratory Data Analysis' course project 1 (fixed locale: Sys.setlocale(LC_TIME, English))

--- Data-Science/Exploratory-Data-Analysis/course-project-1/plot4.R
+++ Data-Science/Exploratory-Data-Analysis/course-project-1/plot4.R
@@ -14,12 +14,20 @@ df$Date = as.Date(df$Date, format="%d/%m/%Y")
 
 
 # Plot the graph and save it on disk
-temp <- par(mfrow= c(1,2))
-
 png(file="plot4.png", width= 480, height= 480)
 
+temp <- par(mfrow= c(2,2))
+
 plot(df$Time, df$Global_active_power, type="l", main='', xlab='', ylab='Global Active Power')
-#plot(df$Time, df$Voltage, type="l", main='', xlab='datetim')
+plot(df$Time, df$Voltage, type="l", main='', xlab='datetime', ylab="Voltage")
+plot(df$Time, df$Sub_metering_1, type="l", 
+     main='', 
+     xlab='',
+     ylab='Energy sub metering')
+lines(df$Time, df$Sub_metering_2, col= 'red')
+lines(df$Time, df$Sub_metering_3, col= 'blue')
+legend("topright",  lty=c(1,1),  col= c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+plot(df$Time, df$Global_reactive_power, type="l", main='', xlab='datetime', ylab="Global_reactive_power")
 
 dev.off()
 
