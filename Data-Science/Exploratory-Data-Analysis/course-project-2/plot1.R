a479394756ef3b5fb779966c418e4e28ad097ee7
tssp <tssp@web.de>
2014-06-20 17:31:23 +0200

    course 'Exploratory Data Analysis' course project 2 (work in progress)

---
+++ Data-Science/Exploratory-Data-Analysis/course-project-2/plot1.R
@@ -0,0 +1,23 @@
+# Load data
+NEI <- readRDS("summarySCC_PM25.rds")
+
+# Make some preparation
+library(data.table)
+
+data <- as.data.table(NEI)
+data[, c("fips", "SCC", "Pollutant", "type"):=NULL]
+
+data <- data[, lapply(.SD, sum), by = c("year")]
+data$Emissions= data$Emissions / 1000
+
+# Plot the graph and save it on disk
+
+
+library(ggplot2)
+
+png(file="plot1.png", width= 480, height= 480)
+qplot(year, Emissions, data= data, geom= "line") + 
+  labs(title="Total Emissions USA") + 
+  labs(y = expression(PM[2.5] * " in kilo tons"), x = "Year")
+dev.off()
+
