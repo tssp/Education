3d1daef64cab67da7c3735636f699b8e78bb22f4
tssp <tssp@web.de>
2014-06-22 11:58:17 +0200

    course 'Exploratory Data Analysis' course project 2 (work in progress)

--- Data-Science/Exploratory-Data-Analysis/course-project-2/plot5.R
+++ Data-Science/Exploratory-Data-Analysis/course-project-2/plot5.R
@@ -9,20 +9,18 @@ library(data.table)
 data <- as.data.table(NEI)
 
 # Find relevant SCC identifiers
-x <- SCC[grep("(C|c)ombustion", SCC$SCC.Level.One), "SCC"]
-y <- SCC[grep("(C|c)oal", SCC$SCC.Level.One), "SCC"]
+x <- SCC[grep("Highway Vehicles", SCC$SCC.Level.Two), "SCC"]
 
-data <- data[(data$SCC %in% x | data$SCC %in% y), ] 
+data <- data[ data$fips == "24510", ]
+data <- data[data$SCC %in% x, ] 
 data[, c("fips", "SCC", "Pollutant", "type"):=NULL]
 
 data <- data[, lapply(.SD, sum), by = c("year")]
-data$Emissions = data$Emissions / 1000
-
 # Plot the graph and save it on disk
-png(file="plot4.png", width= 1024, height= 1024)
+png(file="plot5.png", width= 1024, height= 1024)
 qplot(year, Emissions, data= data, geom= "line") + 
-  labs(title="Total Emissions from coal combustion-related sources") + 
-  labs(y = expression(PM[2.5] * " in kilo tons"), x = "Year")
+  labs(title="Total Emissions from motor-vehicle sources in Baltimore City") + 
+  labs(y = expression(PM[2.5] * " in tons"), x = "Year")
 
 dev.off()
 
