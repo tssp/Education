8369a62b0a99ce82d32088ba911696479ccc04aa
tssp <tssp@web.de>
2015-04-15 01:01:37 +0200

    course 'Reactive-Programming' release candidate

--- Principles-of-Reactive-Programming/Quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ Principles-of-Reactive-Programming/Quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,13 +9,73 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  property("min1") = forAll { a: Int =>
+
+  lazy val genHeap: Gen[H] = for {
+    v <- arbitrary[Int]
+    h <- oneOf(const(empty), genHeap)
+  } yield insert(v, h)
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  val nonEmptyHeaps = genHeap suchThat (!isEmpty(_))
+
+  property("single min") = forAll { a: Int =>
     val h = insert(a, empty)
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("delete min") = forAll { a: Int =>
+    val h = deleteMin(insert(a, empty))
 
-  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+    isEmpty(h)
+  }
+
+  property("insert non-empty") = forAll(nonEmptyHeaps, arbitrary[Int]) { (h: H, v:Int) =>
+
+    val min= Math.min(findMin(h), v)
+
+    findMin(insert(v, h)) == min
+  }
+
+
+  property("dual min") = forAll { (a: Int, b:Int) =>
+
+    val h = insert(b, insert(a, empty))
+
+    val min= Math.min(a, b)
+
+    findMin(h) == min
+  }
+
+  property("melding non-empty min") = forAll(nonEmptyHeaps, nonEmptyHeaps) { (h1: H, h2:H) =>
+
+    val h = meld(h1, h2)
+
+    val min= Math.min(findMin(h1), findMin(h2))
+
+    findMin(h) == min
+  }
+
+
+
+  property("comparing non-empty min") = forAll(nonEmptyHeaps, nonEmptyHeaps) { (h1: H, h2:H) =>
+
+    def compareHeaps(hh1: H, hh2: H): Boolean =
+      if(isEmpty(hh1) && isEmpty(hh2)) true
+      else findMin(hh1) == findMin(hh2) && compareHeaps(deleteMin(hh1), deleteMin(hh2))
+
+    compareHeaps(meld(h1, h2), meld(deleteMin(h1), insert(findMin(h1), h2)))
+  }
+
+  property("non-empty sorted") = forAll(nonEmptyHeaps) { h: H =>
+
+    def heap2list(h: H):List[Int] =
+      if(isEmpty(h)) Nil
+      else findMin(h)::heap2list(deleteMin(h))
+
+    val xs = heap2list(h)
+
+    xs.sorted == xs
+  }
 
 }
