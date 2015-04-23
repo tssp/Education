ca0c934e10ef1be1230e255ac944fe091f6f7d6a
tssp <tssp@web.de>
2015-04-23 22:13:19 +0200

    course 'Reactive-Programming' work in progress

--- Principles-of-Reactive-Programming/Calculator/src/main/scala/calculator/Calculator.scala
+++ Principles-of-Reactive-Programming/Calculator/src/main/scala/calculator/Calculator.scala
@@ -9,20 +9,40 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = namedExpressions.map {
+    case (name, expression) =>
+
+      val computation = Var(0.0)
+      val expr = expression()
+
+      computation() = {
+
+        //println("ComputeValues "+name)
+        //println("Expression    "+expr)
+
+        eval(expr, namedExpressions)
+      }
+
+      (name, computation)
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+
+    case Literal(v)   => v
+    case Ref(name)    => eval(getReferenceExpr(name, references), references)
+    case Plus(a, b)   => eval(a, references) + eval(b, references)
+    case Minus(a, b)  => eval(a, references) - eval(b, references)
+    case Times(a, b)  => eval(a, references) * eval(b, references)
+    case Divide(a, b) => eval(a, references) / eval(b, references)
   }
 
-  /** Get the Expr for a referenced variables.
+  /**
+   * Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+                               references: Map[String, Signal[Expr]]) = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
