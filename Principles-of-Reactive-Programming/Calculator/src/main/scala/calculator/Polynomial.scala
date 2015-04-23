2f5817642bea87b2fb222b38939f83235abecb9a
tssp <tssp@web.de>
2015-04-23 21:57:43 +0200

    course 'Reactive-Programming' work in progress

--- Principles-of-Reactive-Programming/Calculator/src/main/scala/calculator/Polynomial.scala
+++ Principles-of-Reactive-Programming/Calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,50 @@
 package calculator
 
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] = {
+
+    val delta = Var(0.0)
+
+    delta() = {
+
+      // Δ = b² - 4ac
+
+      val aa = a()
+      val bb = b()
+      val cc = c()
+
+      (bb * bb) - (4 * aa * cc)
+    }
+
+    delta
   }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+  def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+
+    val roots = Var(Set.empty[Double])
+
+    roots() = {
+
+      // (-b ± √Δ) / (2a)
+
+      val dd = delta()
+      val aa = a()
+      val bb = b()
+      val cc = c()
+
+      if (dd < 0)
+        Set(0)
+      else {
+
+        val r1 = ((bb * -1) + Math.sqrt(dd)) / (2 * aa)
+        val r2 = ((bb * -1) - Math.sqrt(dd)) / (2 * aa)
+
+        Set(r1, r2)
+
+      }
+
+    }
+
+    roots
   }
 }
