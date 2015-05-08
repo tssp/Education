5ec7ed969a123eca9700f715399088d69620ab92
tssp <tssp@web.de>
2015-05-08 10:40:19 +0200

    course 'Reactive-Programming' work in progress

--- Principles-of-Reactive-Programming/NodeScala/src/main/scala/nodescala/Main.scala
+++ Principles-of-Reactive-Programming/NodeScala/src/main/scala/nodescala/Main.scala
@@ -4,7 +4,7 @@ import scala.language.postfixOps
 import scala.concurrent._
 import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
-import scala.async.Async.{async, await}
+import scala.async.Async.{ async, await }
 
 object Main {
 
@@ -25,17 +25,17 @@ object Main {
     // TO IMPLEMENT
     // 3. create a future that completes after 20 seconds
     //    and continues with a `"Server timeout!"` message
-    val timeOut: Future[String] = ???
+    val timeOut: Future[String] = Future.delay(20 seconds).continueWith { _ => "Server timeout!" }
 
     // TO IMPLEMENT
     // 4. create a future that completes when either 20 seconds elapse
     //    or the user enters some text and presses ENTER
-    val terminationRequested: Future[String] = ???
+    val terminationRequested: Future[String] = userInterrupted.continueWith { Await.result(_, 10 seconds) }
 
     // TO IMPLEMENT
     // 5. unsubscribe from the server
     terminationRequested onSuccess {
-      case msg => ???
+      case msg => myServerSubscription.unsubscribe()
     }
   }
 
