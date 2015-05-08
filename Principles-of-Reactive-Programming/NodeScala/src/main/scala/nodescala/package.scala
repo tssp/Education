7526a9e4abb6c7c8b7c87cc43b0afd8d34da2422
tssp <tssp@web.de>
2015-05-08 10:23:10 +0200

    course 'Reactive-Programming' work in progress

--- Principles-of-Reactive-Programming/NodeScala/src/main/scala/nodescala/package.scala
+++ Principles-of-Reactive-Programming/NodeScala/src/main/scala/nodescala/package.scala
@@ -89,7 +89,14 @@ package object nodescala {
     /**
      * Creates a cancellable context for an execution and runs it.
      */
-    def run()(f: CancellationToken => Future[Unit]): Subscription = ???
+    def run()(f: CancellationToken => Future[Unit]): Subscription = {
+      
+      val ctSource= CancellationTokenSource()
+      
+      f(ctSource.cancellationToken)
+      
+      ctSource
+    }
 
   }
 
