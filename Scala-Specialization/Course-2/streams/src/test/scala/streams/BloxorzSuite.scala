6cd9f516352b0920c54f60d18476db75af408a05
tssp <tim@coding-me.com>
2016-08-19 23:22:08 +0200

    Assignment 1 (Scala Specialization/Functional Program Design in Scala)

---
+++ Scala-Specialization/Course-2/streams/src/test/scala/streams/BloxorzSuite.scala
@@ -0,0 +1,94 @@
+package streams
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import Bloxorz._
+
+@RunWith(classOf[JUnitRunner])
+class BloxorzSuite extends FunSuite {
+
+  trait SolutionChecker extends GameDef with Solver with StringParserTerrain {
+    /**
+     * This method applies a list of moves `ls` to the block at position
+     * `startPos`. This can be used to verify if a certain list of moves
+     * is a valid solution, i.e. leads to the goal.
+     */
+    def solve(ls: List[Move]): Block =
+      ls.foldLeft(startBlock) { case (block, move) => move match {
+        case Left => block.left
+        case Right => block.right
+        case Up => block.up
+        case Down => block.down
+      }
+    }
+  }
+
+  trait Level1 extends SolutionChecker {
+      /* terrain for level 1*/
+
+    val level =
+    """ooo-------
+      |oSoooo----
+      |ooooooooo-
+      |-ooooooooo
+      |-----ooToo
+      |------ooo-""".stripMargin
+
+    val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
+  }
+
+
+	test("terrain function level 1") {
+    new Level1 {
+      assert(terrain(Pos(0,0)), "0,0")
+      assert(terrain(Pos(1,1)), "1,1") // start
+      assert(terrain(Pos(4,7)), "4,7") // goal
+      assert(terrain(Pos(5,8)), "5,8")
+      assert(!terrain(Pos(5,9)), "5,9")
+      assert(terrain(Pos(4,9)), "4,9")
+      assert(!terrain(Pos(6,8)), "6,8")
+      assert(!terrain(Pos(4,11)), "4,11")
+      assert(!terrain(Pos(-1,0)), "-1,0")
+      assert(!terrain(Pos(0,-1)), "0,-1")
+    }
+  }
+
+  test("block functions") {
+    new Level1 {
+
+      assert(Block(Pos(1, 1), Pos(1, 1)).isStanding, "Block is not standing")
+      assert(!Block(Pos(1, 1), Pos(1, 2)).isStanding, "Block is standing")
+
+
+      assert(Block(Pos(0, 0), Pos(1, 0)).isLegal, "Block is not legal")
+      assert(!Block(Pos(2, 0), Pos(3, 0)).isLegal, "Block is legal")
+      assert(!Block(Pos(3, 0), Pos(3, 0)).isLegal, "Block is legal")
+
+
+    }
+  }
+
+	test("findChar level 1") {
+    new Level1 {
+      assert(startPos == Pos(1,1))
+    }
+  }
+
+
+	test("optimal solution for level 1") {
+    new Level1 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+
+	test("optimal solution length for level 1") {
+    new Level1 {
+      assert(solution.length == optsolution.length)
+    }
+  }
+
+}
